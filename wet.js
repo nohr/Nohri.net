3158f31fba474e112a604f413ac8296b553f7623
Aite <aite037@gmail.com>
2019-11-06 06:56:16 -0500

    Add files via upload

--- wet.js
+++ wet.js
@@ -223,7 +223,7 @@ const fx = new TextScramble(el);
 let counter = 0;
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 10000);
+    setTimeout(next, 24000);
   });
   counter = (counter + 1) % phrases.length;
 };
